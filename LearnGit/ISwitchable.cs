fb7ffb9e3da998b334ce081366e3b7b7f8bd267f
Владимир Солдатов <sold.94@mail.ru>
2020-02-08 03:12:58 +0700

    Add LightController

---
+++ LearnGit/ISwitchable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnGit
+{
+    interface ISwitchable
+    {
+        void Switch();
+    }
+}
---
+++ LearnGit/LightController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnGit
+{
+    class LightController : ISwitchable
+    {
+        private readonly Light light;
+
+        public LightController(Light light)
+        {
+            this.light = light;
+        }
+        public void Switch()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
